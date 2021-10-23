4e5a3baed40febe3cdea6a7e68b5ead5e71959e8
devAYZ <afatokimi@getcarbon.co>
2021-10-23 23:19:08 +0100

    add table cell

---
+++ tableSectioning/tableSectioning/cell/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  tableSectioning
+//
+//  Created by Ayokunle Fatokimi on 23/10/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
